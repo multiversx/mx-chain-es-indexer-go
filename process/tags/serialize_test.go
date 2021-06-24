a46acf7eb4839345f82e81bf667acc8ae7ab4adb
miiu96 <mihai9625@gmail.com>
2021-06-24 17:14:56 +0300

    unit test serialize

---
+++ process/tags/serialize_test.go
@@ -0,0 +1,29 @@
+package tags
+
+import (
+	"testing"
+
+	"github.com/ElrondNetwork/elastic-indexer-go/data"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTagsCount_Serialize(t *testing.T) {
+	t.Parallel()
+
+	tagsC := NewTagsCount()
+
+	attributes := &data.Attributes{
+		"tags": []string{"Art"},
+	}
+
+	tagsC.ExtractTagsFromAttributes(attributes)
+	tagsC.ExtractTagsFromAttributes(attributes)
+
+	buff, err := tagsC.Serialize()
+	require.Nil(t, err)
+
+	expected := `{ "index" : { "_id" : "Art" } }
+{ "count" : 2 }
+`
+	require.Equal(t, expected, buff[0].String())
+}
