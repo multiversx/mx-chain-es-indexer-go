8e2fa1684601a1c38f1c3e0dd5fea411973f62be
miiu96 <mihai9625@gmail.com>
2021-06-29 16:02:39 +0300

    unit test logsAndEventsProcessor

--- process/logsevents/logsAndEventsProcessor_test.go
+++ process/logsevents/logsAndEventsProcessor_test.go
@@ -1,10 +1,15 @@
 package logsevents
 
 import (
+	"math/big"
 	"testing"
 
 	elasticIndexer "github.com/ElrondNetwork/elastic-indexer-go"
+	"github.com/ElrondNetwork/elastic-indexer-go/data"
 	"github.com/ElrondNetwork/elastic-indexer-go/mock"
+	"github.com/ElrondNetwork/elrond-go/core"
+	nodeData "github.com/ElrondNetwork/elrond-go/data"
+	"github.com/ElrondNetwork/elrond-go/data/transaction"
 	"github.com/stretchr/testify/require"
 )
 
@@ -20,4 +25,42 @@ func TestNewLogsAndEventsProcessor(t *testing.T) {
 	proc, err := NewLogsAndEventsProcessor(&mock.ShardCoordinatorMock{}, &mock.PubkeyConverterMock{})
 	require.NotNil(t, proc)
 	require.Nil(t, err)
+}
+
+func TestLogsAndEventsProcessor_PrepareLogsForDB(t *testing.T) {
+	t.Parallel()
+
+	logsAndEvents := map[string]nodeData.LogHandler{
+		"wrong": nil,
+
+		"txHash": &transaction.Log{
+			Address: []byte("address"),
+			Events: []*transaction.Event{
+				{
+					Address:    []byte("addr"),
+					Identifier: []byte(core.BuiltInFunctionESDTNFTTransfer),
+					Topics:     [][]byte{[]byte("my-token"), big.NewInt(0).SetUint64(1).Bytes(), []byte("receiver")},
+				},
+			},
+		},
+
+		"noEvents": &transaction.Log{
+			Address: []byte("sender"),
+		},
+	}
+
+	proc, _ := NewLogsAndEventsProcessor(&mock.ShardCoordinatorMock{}, mock.NewPubkeyConverterMock(32))
+
+	logsDB := proc.PrepareLogsForDB(logsAndEvents)
+	require.Equal(t, &data.Logs{
+		ID:      "747848617368",
+		Address: "61646472657373",
+		Events: []*data.Event{
+			{
+				Address:    "61646472",
+				Identifier: core.BuiltInFunctionESDTNFTTransfer,
+				Topics:     [][]byte{[]byte("my-token"), big.NewInt(0).SetUint64(1).Bytes(), []byte("receiver")},
+			},
+		},
+	}, logsDB[0])
 }
