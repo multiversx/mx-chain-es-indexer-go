607d5e7a1fdd3890346332ec2901601d0ec857dd
miiu96 <mihai9625@gmail.com>
2021-07-20 17:14:40 +0300

    extra check

--- process/tags/serialize.go
+++ process/tags/serialize.go
@@ -11,6 +11,10 @@ import (
 func (tc *tagsCount) Serialize() ([]*bytes.Buffer, error) {
 	buffSlice := data.NewBufferSlice()
 	for tag, count := range tc.tags {
+		if tag == "" {
+			continue
+		}
+
 		meta := []byte(fmt.Sprintf(`{ "update" : { "_id" : "%s", "_type" : "_doc" } }%s`, tag, "\n"))
 		serializedDataStr := fmt.Sprintf(`{"script": {"source": "ctx._source.count += params.count","lang": "painless","params": {"count": %d}},"upsert": {"count": %d}}`, count, count)
 
