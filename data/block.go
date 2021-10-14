7e360f6a2a4849e14dec4f439fcb3be25759608b
miiu96 <mihai9625@gmail.com>
2021-10-14 17:26:03 +0300

    fixes after merge

--- data/block.go
+++ data/block.go
@@ -8,26 +8,43 @@ import (
 //  to be saved for a block. It has all the default fields
 //  plus some extra information for ease of search and filter
 type Block struct {
-	Nonce                 uint64        `json:"nonce"`
-	Round                 uint64        `json:"round"`
-	Epoch                 uint32        `json:"epoch"`
-	Hash                  string        `json:"-"`
-	MiniBlocksHashes      []string      `json:"miniBlocksHashes"`
-	NotarizedBlocksHashes []string      `json:"notarizedBlocksHashes"`
-	Proposer              uint64        `json:"proposer"`
-	Validators            []uint64      `json:"validators"`
-	PubKeyBitmap          string        `json:"pubKeyBitmap"`
-	Size                  int64         `json:"size"`
-	SizeTxs               int64         `json:"sizeTxs"`
-	Timestamp             time.Duration `json:"timestamp"`
-	StateRootHash         string        `json:"stateRootHash"`
-	PrevHash              string        `json:"prevHash"`
-	ShardID               uint32        `json:"shardId"`
-	TxCount               uint32        `json:"txCount"`
-	AccumulatedFees       string        `json:"accumulatedFees"`
-	DeveloperFees         string        `json:"developerFees"`
-	EpochStartBlock       bool          `json:"epochStartBlock"`
-	SearchOrder           uint64        `json:"searchOrder"`
+	Nonce                 uint64          `json:"nonce"`
+	Round                 uint64          `json:"round"`
+	Epoch                 uint32          `json:"epoch"`
+	Hash                  string          `json:"-"`
+	MiniBlocksHashes      []string        `json:"miniBlocksHashes"`
+	NotarizedBlocksHashes []string        `json:"notarizedBlocksHashes"`
+	Proposer              uint64          `json:"proposer"`
+	Validators            []uint64        `json:"validators"`
+	PubKeyBitmap          string          `json:"pubKeyBitmap"`
+	Size                  int64           `json:"size"`
+	SizeTxs               int64           `json:"sizeTxs"`
+	Timestamp             time.Duration   `json:"timestamp"`
+	StateRootHash         string          `json:"stateRootHash"`
+	PrevHash              string          `json:"prevHash"`
+	ShardID               uint32          `json:"shardId"`
+	TxCount               uint32          `json:"txCount"`
+	AccumulatedFees       string          `json:"accumulatedFees"`
+	DeveloperFees         string          `json:"developerFees"`
+	EpochStartBlock       bool            `json:"epochStartBlock"`
+	SearchOrder           uint64          `json:"searchOrder"`
+	EpochStartInfo        *EpochStartInfo `json:"epochStartInfo,omitempty"`
+	GasConsumed           uint64          `json:"gasConsumed"`
+	GasRefunded           uint64          `json:"gasRefunded"`
+	GasPenalized          uint64          `json:"gasPenalized"`
+	MaxGasLimit           uint64          `json:"maxGasLimit"`
+}
+
+// EpochStartInfo is a structure that hold information about epoch start meta block
+type EpochStartInfo struct {
+	TotalSupply                      string `json:"totalSupply"`
+	TotalToDistribute                string `json:"totalToDistribute"`
+	TotalNewlyMinted                 string `json:"totalNewlyMinted"`
+	RewardsPerBlock                  string `json:"rewardsPerBlock"`
+	RewardsForProtocolSustainability string `json:"rewardsForProtocolSustainability"`
+	NodePrice                        string `json:"nodePrice"`
+	PrevEpochStartRound              uint64 `json:"prevEpochStartRound"`
+	PrevEpochStartHash               string `json:"prevEpochStartHash"`
 }
 
 // Miniblock is a structure containing miniblock information
