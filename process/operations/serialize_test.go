40cb8a3ab32fb28a42d80e42da7d406466c000bb
miiu <mihai9625@gmail.com>
2021-12-07 10:50:48 +0200

    unit test serialize scrs

--- process/operations/serialize_test.go
+++ process/operations/serialize_test.go
@@ -1,1 +1,34 @@
 package operations
+
+import (
+	"testing"
+
+	"github.com/ElrondNetwork/elastic-indexer-go/data"
+	"github.com/ElrondNetwork/elastic-indexer-go/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOperationsProcessor_SerializeSCRS(t *testing.T) {
+	t.Parallel()
+
+	op, _ := NewOperationsProcessor(false, &mock.ShardCoordinatorMock{})
+
+	scrs := []*data.ScResult{
+		{
+			SenderShard:   0,
+			ReceiverShard: 1,
+		},
+		{
+			SenderShard:   2,
+			ReceiverShard: 0,
+		},
+	}
+
+	res, err := op.SerializeSCRS(scrs)
+	require.Nil(t, err)
+	require.Equal(t, `{"update":{"_id":"", "_type": "_doc"}}
+{"script":{"source":"return"},"upsert":{"nonce":0,"gasLimit":0,"gasPrice":0,"value":"","sender":"","receiver":"","senderShard":0,"receiverShard":1,"prevTxHash":"","originalTxHash":"","callType":"","timestamp":0}}
+{ "index" : { "_id" : "", "_type" : "_doc" } }
+{"nonce":0,"gasLimit":0,"gasPrice":0,"value":"","sender":"","receiver":"","senderShard":2,"receiverShard":0,"prevTxHash":"","originalTxHash":"","callType":"","timestamp":0}
+`, res[0].String())
+}
