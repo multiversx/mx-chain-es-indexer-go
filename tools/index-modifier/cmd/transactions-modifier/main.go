228bed942a84d0dc46d54da2718feaaba82c30a8
miiu <mihai9625@gmail.com>
2021-12-21 15:19:21 +0200

    fixes after review

--- tools/index-modifier/cmd/transactions-modifier/main.go
+++ tools/index-modifier/cmd/transactions-modifier/main.go
@@ -23,7 +23,7 @@ func main() {
 		panic("cannot create transactions modifier: " + err.Error())
 	}
 
-	err = indexModifier.AlterIndex("", "", txsModifier.Modify)
+	err = indexModifier.AlterIndex("transactions", "transactions", txsModifier.Modify)
 	if err != nil {
 		panic("cannot modify index: " + err.Error())
 	}
