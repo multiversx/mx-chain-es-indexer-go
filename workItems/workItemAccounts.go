87a6eaa5c8d438dfbe65624cc1ef20859e9b3302
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2022-02-09 19:23:32 +0200

    small fix

--- workItems/workItemAccounts.go
+++ workItems/workItemAccounts.go
@@ -26,7 +26,7 @@ func NewItemAccounts(
 
 // Save will save information about an account
 func (wiv *itemAccounts) Save() error {
-	accounts := make([]*data.Account, len(wiv.accounts))
+	accounts := make([]*data.Account, 0, len(wiv.accounts))
 	for _, account := range wiv.accounts {
 		accounts = append(accounts, &data.Account{
 			UserAccount: account,
