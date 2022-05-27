9d8250f27a6c3b8c3d7f162c786c7a033999737e
Iuga Mihai <mihai9625@gmail.com>
2022-05-27 16:13:01 +0300

    fix integration tests 1

--- data/tokens.go
+++ data/tokens.go
@@ -40,7 +40,7 @@ type TokenInfo struct {
 	Token             string           `json:"token,omitempty"`
 	Issuer            string           `json:"issuer,omitempty"`
 	CurrentOwner      string           `json:"currentOwner,omitempty"`
-	NumDecimals       uint64           `json:"numDecimals"`
+	NumDecimals       uint64           `json:"numDecimals,omitempty"`
 	Type              string           `json:"type,omitempty"`
 	Nonce             uint64           `json:"nonce,omitempty"`
 	Timestamp         time.Duration    `json:"timestamp,omitempty"`
