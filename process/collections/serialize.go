6a279d9aefd2a69d1802fa3536d9554cc9b774e8
Iuga Mihai <mihai9625@gmail.com>
2022-06-24 12:16:55 +0300

    integration test

--- process/collections/serialize.go
+++ process/collections/serialize.go
@@ -19,7 +19,9 @@ func ExtractAndSerializeCollectionsData(
 	for _, acct := range accountsESDT {
 		shouldIgnore := acct.Type != core.NonFungibleESDT && acct.Type != core.SemiFungibleESDT
 		if shouldIgnore {
-			continue
+			if acct.Balance != "0" || acct.TokenNonce == 0 {
+				continue
+			}
 		}
 
 		nonceBig := big.NewInt(0).SetUint64(acct.TokenNonce)
@@ -27,23 +29,25 @@ func ExtractAndSerializeCollectionsData(
 
 		meta := []byte(fmt.Sprintf(`{ "update" : {"_index":"%s", "_id" : "%s" } }%s`, index, acct.Address, "\n"))
 		codeToExecute := `
-			if (params.value != '0') {
+			if ((ctx.op == 'create') && (params.value == '0')) {
+				ctx.op = 'noop';
+			} else if (params.value != '0') {
 				if (!ctx._source.containsKey(params.col)) {
 					ctx._source[params.col] = new HashMap();
 				}
 				ctx._source[params.col][params.nonce] = params.value
 			} else {
 				if (ctx._source.containsKey(params.col)) {
-					ctx._source[params.col].delete(params.nonce);
+					ctx._source[params.col].remove(params.nonce);
 					if (ctx._source[params.col].length == 0) {
-						ctx._source.delete(params.col)
+						ctx._source.remove(params.col)
 					}
 				}
 			}
 `
 
 		collection := fmt.Sprintf(`{"%s":{"%s": "%s"}}`, acct.TokenName, hexEncodedNonce, acct.Balance)
-		serializedDataStr := fmt.Sprintf(`{"script": {`+
+		serializedDataStr := fmt.Sprintf(`{"scripted_upsert": true, "script": {`+
 			`"source": "%s",`+
 			`"lang": "painless",`+
 			`"params": { "col": "%s", "nonce": "%s", "value": "%s"}},`+
