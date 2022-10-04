20d1b438516b0d1918ae3bd992d30f3c3c5fb239
Iuga Mihai <mihai9625@gmail.com>
2022-10-04 15:50:49 +0300

    delete also from tokens index

--- process/logsevents/nftsProcessor.go
+++ process/logsevents/nftsProcessor.go
@@ -94,6 +94,15 @@ func (np *nftsProcessor) processEvent(args *argsProcessEvent) argOutputProcessEv
 		}
 	}
 
+	if eventIdentifier == core.BuiltInFunctionESDTWipe {
+		args.tokensSupply.Add(&data.TokenInfo{
+			Token:      token,
+			Identifier: identifier,
+			Timestamp:  time.Duration(args.timestamp),
+			Nonce:      nonceBig.Uint64(),
+		})
+	}
+
 	args.accounts.Add(encodedReceiver, &data.AlteredAccount{
 		IsNFTOperation:  true,
 		TokenIdentifier: token,
