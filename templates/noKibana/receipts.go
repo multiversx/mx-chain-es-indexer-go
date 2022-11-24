b6c0ef8bfe6fd7108d8772454d7e3583440e5bea
Iuga Mihai <mihai9625@gmail.com>
2022-11-24 14:50:55 +0200

    fix receipts

--- templates/noKibana/receipts.go
+++ templates/noKibana/receipts.go
@@ -17,23 +17,25 @@ var Receipts = Object{
 			},
 		},
 	},
-	"properties": Object{
-		"data": Object{
-			"type": "keyword",
-		},
-		"sender": Object{
-			"type": "keyword",
-		},
-		"timestamp": Object{
-			"type":   "date",
-			"format": "epoch_second",
-		},
-		"txHash": Object{
-			"type": "keyword",
-		},
-		"value": Object{
-			"index": "false",
-			"type":  "keyword",
+	"mappings": Object{
+		"properties": Object{
+			"data": Object{
+				"type": "keyword",
+			},
+			"sender": Object{
+				"type": "keyword",
+			},
+			"timestamp": Object{
+				"type":   "date",
+				"format": "epoch_second",
+			},
+			"txHash": Object{
+				"type": "keyword",
+			},
+			"value": Object{
+				"index": "false",
+				"type":  "keyword",
+			},
 		},
 	},
 }
