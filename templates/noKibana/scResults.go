1375ea340bc77f359be398133293d6956695cb99
Iuga Mihai <mihai9625@gmail.com>
2022-11-24 15:56:06 +0200

    scresults, tags

--- templates/noKibana/scResults.go
+++ templates/noKibana/scResults.go
@@ -19,18 +19,87 @@ var SCResults = Object{
 	},
 	"mappings": Object{
 		"properties": Object{
+			"callType": Object{
+				"type": "keyword",
+			},
+			"code": Object{
+				"index": "false",
+				"type":  "keyword",
+			},
+			"data": Object{
+				"type": "keyword",
+			},
+			"esdtValues": Object{
+				"type": "keyword",
+			},
+			"function": Object{
+				"type": "keyword",
+			},
+			"gasLimit": Object{
+				"type": "long",
+			},
+			"gasPrice": Object{
+				"index": "false",
+				"type":  "long",
+			},
+			"hasOperations": Object{
+				"type": "boolean",
+			},
+			"miniBlockHash": Object{
+				"type": "keyword",
+			},
+			"nonce": Object{
+				"type": "long",
+			},
+			"operation": Object{
+				"type": "keyword",
+			},
+			"originalSender": Object{
+				"type": "keyword",
+			},
+			"originalTxHash": Object{
+				"type": "keyword",
+			},
+			"prevTxHash": Object{
+				"type": "keyword",
+			},
+			"receiver": Object{
+				"type": "keyword",
+			},
+			"receiverShard": Object{
+				"type": "long",
+			},
+			"receivers": Object{
+				"type": "keyword",
+			},
+			"receiversShardIDs": Object{
+				"type": "long",
+			},
+			"relayedValue": Object{
+				"index": "false",
+				"type":  "keyword",
+			},
+			"relayerAddr": Object{
+				"type": "keyword",
+			},
+			"returnMessage": Object{
+				"type": "text",
+			},
+			"sender": Object{
+				"type": "keyword",
+			},
+			"senderShard": Object{
+				"type": "long",
+			},
 			"timestamp": Object{
 				"type":   "date",
 				"format": "epoch_second",
 			},
-			"nonce": Object{
-				"type": "double",
+			"tokens": Object{
+				"type": "keyword",
 			},
-			"gasLimit": Object{
-				"type": "double",
-			},
-			"gasPrice": Object{
-				"type": "double",
+			"value": Object{
+				"type": "keyword",
 			},
 		},
 	},
