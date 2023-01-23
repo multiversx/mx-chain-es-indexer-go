84e2ce55f847385fcacb7150ed8ec093c78a7998
Iuga Mihai <mihai9625@gmail.com>
2023-01-23 16:46:48 +0200

    fix for es 8.x

--- process/elasticproc/logsevents/serializeDelegators.go
+++ process/elasticproc/logsevents/serializeDelegators.go
@@ -44,7 +44,7 @@ func (lep *logsAndEventsProcessor) PrepareDelegatorsQueryInCaseOfRevert(timestam
 	{
 	  "query": {
 		"match": {
-		  "timestamp": %d
+		  "timestamp": "%d"
 		}
 	  },
 	  "script": {
