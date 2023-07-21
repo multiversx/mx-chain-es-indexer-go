60fbbb9b97948c8f597331ba21574da09acfe03f
Iuga Mihai <mihai9625@gmail.com>
2023-07-21 11:55:58 +0300

    fix test

--- metrics/statusMetrics_test.go
+++ metrics/statusMetrics_test.go
@@ -27,14 +27,6 @@ func TestStatusMetrics_AddIndexingDataAndGetMetrics(t *testing.T) {
 		Duration:   15,
 	})
 
-	topic2 := "test2_2"
-	statusMetricsHandler.AddIndexingData(ArgsAddIndexingData{
-		GotError:   true,
-		MessageLen: 100,
-		Topic:      topic2,
-		Duration:   12,
-	})
-
 	metrics := statusMetricsHandler.GetMetrics()
 	require.Equal(t, &request.MetricsResponse{
 		OperationsCount:   2,
@@ -42,13 +34,7 @@ func TestStatusMetrics_AddIndexingDataAndGetMetrics(t *testing.T) {
 		TotalIndexingTime: 27,
 		TotalData:         322,
 	}, metrics[topic1])
-	require.Equal(t, &request.MetricsResponse{
-		OperationsCount:   1,
-		ErrorsCount:       1,
-		TotalIndexingTime: 12,
-		TotalData:         100,
-	}, metrics[topic2])
 
 	prometheusMetrics := statusMetricsHandler.GetMetricsForPrometheus()
-	require.Equal(t, "# TYPE test1 counter\ntest1{shardID=\"0\"} 322\n\n# TYPE test1 counter\ntest1{shardID=\"0\"} 1\n\n# TYPE test1 counter\ntest1{shardID=\"0\"} 2\n\n# TYPE test1 counter\ntest1{shardID=\"0\"} 0\n\n# TYPE test2 counter\ntest2{shardID=\"2\"} 100\n\n# TYPE test2 counter\ntest2{shardID=\"2\"} 1\n\n# TYPE test2 counter\ntest2{shardID=\"2\"} 1\n\n# TYPE test2 counter\ntest2{shardID=\"2\"} 0\n\n", prometheusMetrics)
+	require.Equal(t, "# TYPE test1 counter\ntest1{shardID=\"0\"} 322\n\n# TYPE test1 counter\ntest1{shardID=\"0\"} 1\n\n# TYPE test1 counter\ntest1{shardID=\"0\"} 2\n\n# TYPE test1 counter\ntest1{shardID=\"0\"} 0\n\n", prometheusMetrics)
 }
