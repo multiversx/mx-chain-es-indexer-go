8834ef9d4e15c05fc6ed12ed825c667c67167859
Iuga Mihai <mihai9625@gmail.com>
2023-07-21 14:07:43 +0300

    extra check

--- factory/webServerFactory.go
+++ factory/webServerFactory.go
@@ -1,6 +1,7 @@
 package factory
 
 import (
+	"github.com/multiversx/mx-chain-core-go/core/check"
 	"github.com/multiversx/mx-chain-es-indexer-go/api/gin"
 	"github.com/multiversx/mx-chain-es-indexer-go/config"
 	"github.com/multiversx/mx-chain-es-indexer-go/core"
@@ -9,6 +10,10 @@ import (
 
 // CreateWebServer will create a new instance of core.WebServerHandler
 func CreateWebServer(apiConfig config.ApiRoutesConfig, statusMetricsHandler core.StatusMetricsHandler) (core.WebServerHandler, error) {
+	if check.IfNil(statusMetricsHandler) {
+		return nil, core.ErrNilMetricsHandler
+	}
+
 	metricsFacade, err := facade.NewMetricsFacade(statusMetricsHandler)
 	if err != nil {
 		return nil, err
