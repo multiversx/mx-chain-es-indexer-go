fda188fb613b40b22786eef8e76105bdca163981
miiu <mihai9625@gmail.com>
2024-04-30 11:25:23 +0300

    struct

--- config/migrations.go
+++ config/migrations.go
@@ -1,1 +1,16 @@
 package config
+
+type MigrationsConfig struct {
+	SourceCluster struct {
+		URL      string `toml:"url"`
+		UserName string `toml:"username"`
+		Password string `toml:"password"`
+	} `toml:"source-cluster"`
+	Migrations []struct {
+		Start            string `toml:"start"`
+		Name             string `toml:"name"`
+		Description      string `toml:"description"`
+		SourceIndex      string `toml:"source-index"`
+		DestinationIndex string `toml:"destination-index"`
+	} `toml:"migrations"`
+}
