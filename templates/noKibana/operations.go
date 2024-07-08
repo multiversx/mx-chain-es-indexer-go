e632bc2407a4dfdc076e74bfc1bd6f59cb3d4d63
miiu <mihai9625@gmail.com>
2024-07-08 12:04:53 +0300

    new mappings in transaction and operation index

--- templates/noKibana/operations.go
+++ templates/noKibana/operations.go
@@ -169,6 +169,62 @@ var Operations = Object{
 				"index": "false",
 				"type":  "keyword",
 			},
+			"innerTransactions": Object{
+				"properties": Object{
+					"nonce": Object{
+						"type": "double",
+					},
+					"value": Object{
+						"type": "keyword",
+					},
+					"sender": Object{
+						"type": "keyword",
+					},
+					"receiver": Object{
+						"type": "keyword",
+					},
+					"senderUserName": Object{
+						"type": "keyword",
+					},
+					"receiverUsername": Object{
+						"type": "keyword",
+					},
+					"gasLimit": Object{
+						"index": "false",
+						"type":  "double",
+					},
+					"gasPrice": Object{
+						"index": "false",
+						"type":  "double",
+					},
+					"data": Object{
+						"type": "text",
+					},
+					"signature": Object{
+						"index": "false",
+						"type":  "keyword",
+					},
+					"chainID": Object{
+						"type": "keyword",
+					},
+					"version": Object{
+						"type": "long",
+					},
+					"options": Object{
+						"type": "long",
+					},
+					"guardian": Object{
+						"type": "keyword",
+					},
+					"guardianSignature": Object{
+						"index": "false",
+						"type":  "keyword",
+					},
+					"relayer": Object{
+						"type": "keyword",
+					},
+				},
+			},
 		},
 	},
 }
