677e42926c127d943b90956d1e6a00ef78fcedab
miiu <mihai9625@gmail.com>
2025-01-20 15:03:01 +0200

    change urmarshall to remove uuid for json

--- integrationtests/data.go
+++ integrationtests/data.go
@@ -13,3 +13,57 @@ type GenericResponseDB struct {
 	ID     string          `json:"_id"`
 	Source json.RawMessage `json:"_source"`
 }
+
+func (gr *GenericResponseDB) UnmarshalJSON(data []byte) error {
+	type Alias GenericResponseDB
+	aux := &struct {
+		Source json.RawMessage `json:"_source"`
+		*Alias
+	}{
+		Alias: (*Alias)(gr),
+	}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	var sourceMap map[string]interface{}
+	if err := json.Unmarshal(aux.Source, &sourceMap); err != nil {
+		return err
+	}
+
+	delete(sourceMap, "uuid")
+
+	modifiedSource, err := json.Marshal(sourceMap)
+	if err != nil {
+		return err
+	}
+
+	gr.Source = modifiedSource
+	return nil
+}
+
+func (gr *GenericResponse) UnmarshalJSON(data []byte) error {
+	type Alias GenericResponse
+	aux := &struct {
+		Docs []json.RawMessage `json:"docs"`
+		*Alias
+	}{
+		Alias: (*Alias)(gr),
+	}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	gr.Docs = make([]GenericResponseDB, len(aux.Docs))
+	for i, doc := range aux.Docs {
+		var docObj GenericResponseDB
+		if err := json.Unmarshal(doc, &docObj); err != nil {
+			return err
+		}
+		gr.Docs[i] = docObj
+	}
+
+	return nil
+}
