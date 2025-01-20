374a7c951ac67257c81a9c9e50412fffc21b4cec
miiu <mihai9625@gmail.com>
2025-01-20 15:14:17 +0200

    fix integration tests

--- integrationtests/data.go
+++ integrationtests/data.go
@@ -28,14 +28,11 @@ func (gr *GenericResponseDB) UnmarshalJSON(data []byte) error {
 		return err
 	}
 
-	if gr.Source == nil {
+	var sourceMap map[string]interface{}
+	err := json.Unmarshal(aux.Source, &sourceMap)
+	if err != nil {
 		gr.Source = aux.Source
 		return nil
-	}
-
-	var sourceMap map[string]interface{}
-	if err := json.Unmarshal(aux.Source, &sourceMap); err != nil {
-		return err
 	}
 
 	delete(sourceMap, "uuid")
