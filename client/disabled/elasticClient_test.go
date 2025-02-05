9d96aee965ad9bc7d052bc536cdd8205bc865916
axenteoctavian <axente.octavian@gmail.com>
2025-02-05 12:20:22 +0200

    unit test fixes

--- client/disabled/elasticClient_test.go
+++ client/disabled/elasticClient_test.go
@@ -2,6 +2,7 @@ package disabled
 
 import (
 	"bytes"
+	"context"
 	"testing"
 
 	"github.com/multiversx/mx-chain-core-go/core/check"
@@ -15,12 +16,12 @@ func TestDisabledElasticClient_MethodsShouldNotPanic(t *testing.T) {
 	require.False(t, check.IfNil(ec))
 
 	require.NotPanics(t, func() {
-		_ = ec.DoBulkRequest(nil, new(bytes.Buffer), "")
-		_ = ec.DoQueryRemove(nil, "", new(bytes.Buffer))
-		_ = ec.DoMultiGet(nil, make([]string, 0), "", true, nil)
-		_ = ec.DoScrollRequest(nil, "", []byte(""), true, nil)
-		_, _ = ec.DoCountRequest(nil, "", []byte(""))
-		_ = ec.UpdateByQuery(nil, "", new(bytes.Buffer))
+		_ = ec.DoBulkRequest(context.Background(), new(bytes.Buffer), "")
+		_ = ec.DoQueryRemove(context.Background(), "", new(bytes.Buffer))
+		_ = ec.DoMultiGet(context.Background(), make([]string, 0), "", true, nil)
+		_ = ec.DoScrollRequest(context.Background(), "", []byte(""), true, nil)
+		_, _ = ec.DoCountRequest(context.Background(), "", []byte(""))
+		_ = ec.UpdateByQuery(context.Background(), "", new(bytes.Buffer))
 		_ = ec.PutMappings("", new(bytes.Buffer))
 		_ = ec.CheckAndCreateIndex("")
 		_ = ec.CheckAndCreateAlias("", "")
